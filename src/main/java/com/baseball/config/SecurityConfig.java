ddbfef512ced0f9fcec232c8d53d3b0d4329db2e
chunsikman <sb97213@gmail.com>
2024-02-13 14:12:20 +0900

    수정

--- src/main/java/com/baseball/config/SecurityConfig.java
+++ src/main/java/com/baseball/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
                 // .authorizeHttpRequests((auth) -> auth.anyRequest().permitAll())
                 .authorizeHttpRequests((auth) -> auth
                         // 토큰 검증을 하지 않을 요청
-                        .requestMatchers("/auth/**","/kakao/**", "https://kauth.kakao.com/**", "https://kapi/kakao.com/**")
+                        .requestMatchers("/hello","/auth/**","/kakao/**", "https://kauth.kakao.com/**", "https://kapi/kakao.com/**")
                         .permitAll()
                         // 그 외의 모든 요청은 토큰이 있어야 접근 가능
                         .anyRequest().authenticated())
---
+++ src/main/java/com/baseball/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.baseball.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ResponseBody
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "안녕?";
+    }
+}
